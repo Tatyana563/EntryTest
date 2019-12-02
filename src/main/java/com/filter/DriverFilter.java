39073fa41e7a42da5f7672ee9127a45f6ed9e1ba
Tatyana563 <nikolaev.english@gmail.com>
2019-12-02 13:27:58 +0200

    commit 2.12

---
+++ src/main/java/com/filter/DriverFilter.java
@@ -0,0 +1,32 @@
+package com.filter;
+
+import com.domain.Driver;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Objects;
+//check
+@WebFilter(urlPatterns = {"/driver"})
+public class DriverFilter implements Filter {
+    public static final int DRIVER_AGE=18;
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        if(Objects.equals(servletRequest.getContentType(),"application/json")){
+            ObjectMapper mapper = new ObjectMapper();
+            try(BufferedReader reader = servletRequest.getReader()){
+                Driver driver = mapper.readValue(reader, Driver.class);
+                if(driver.getAge()>DRIVER_AGE){
+                    filterChain.doFilter(servletRequest, servletResponse);
+                }
+                else{
+                    final PrintWriter writer = servletResponse.getWriter();
+                    writer.println("Incorrect age of the driver");
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/filter/TruckFilter.java
@@ -0,0 +1,55 @@
+package com.filter;
+
+import com.domain.Track;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Objects;
+
+
+
+@WebFilter(urlPatterns = {"/track"})
+//@WebFilter(servletNames = {"CarServlet"})
+public class TruckFilter implements Filter {
+
+    public static final int MODEL_YEAR = 1990;
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+//update post
+// for json (not from browser line)
+
+        if (Objects.equals(servletRequest.getContentType(), "application/json")) {
+            ObjectMapper mapper = new ObjectMapper();//improve
+            try (BufferedReader reader = servletRequest.getReader()) {
+
+                Track track = mapper.readValue(reader, Track.class);
+
+                if (track.getModelYear() > MODEL_YEAR) {
+
+                    filterChain.doFilter(servletRequest, servletResponse);
+                } else {
+                    final PrintWriter writer = servletResponse.getWriter();
+
+                    writer.println("Incorrect model year");
+                }
+
+            }
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("Create filter for trucks");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Delete filter for trucks");
+    }
+}
